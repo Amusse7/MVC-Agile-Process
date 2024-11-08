9cc8e8993df08fedd130d1d8207452c3ed3277ad
amusse8 <amusse3@my.centennialcollege.ca>
2024-11-08 18:48:09 -0500

    Initial Commit

---
+++ MVC-Agile/Data/MVC_AgileContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Agile.Models;
+
+namespace MVC_Agile.Data
+{
+    public class MVC_AgileContext : DbContext
+    {
+        public MVC_AgileContext (DbContextOptions<MVC_AgileContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Agile.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MVC-Agile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Agile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
